bd3bae1b086a8d2ce0047cf8aef2ee238efb51b4
Yuya Morita <yuya@YuyanoMacBook-Pro.local>
2020-11-19 02:34:00 +0900

    悲願のカメラ #5

--- src/components/pages/video/index.js
+++ src/components/pages/video/index.js
@@ -2,16 +2,55 @@ import React from 'react';
 import WithHeader from '../../Header/with-header';
 import Footer from '../../Footer/index';
 import './styles.css';
+import Webcam from "react-webcam";
+
 
 class CameraPage extends React.Component {
-  
+  enableWebcam = () => this.setState({ webcamEnabled: true });
+  disenableWebcam = () => this.setState({ webcamEnabled: false });
+
+  constructor(props) {
+    super(props);
+    this.state = { webcamEnabled: false };
+  }
   render() {
+    const videoConstraints = {
+      width: 1280,
+      height: 720,
+      facingMode: "user"
+    };
     return (
       <div>
         <WithHeader/>
         <div className="page-title">
           <div className="text">
-            <p>Video Page</p>
+            <div>
+                {(() => {
+                        if (this.state.webcamEnabled){
+                          
+                          return(
+                          <React.Fragment>
+                            <button onClick={this.disenableWebcam}>Disenable webcam</button>
+                            <Webcam 
+                            audio={false}
+                            height={350}
+                            ref={this.setRef}
+                            screenshotFormat="image/jpeg"
+                            width={350}
+                            videoConstraints={videoConstraints}
+                            />
+                          </React.Fragment>
+                          );
+                        }else{
+                          return(
+                            <React.Fragment>
+                              <button onClick={this.enableWebcam}>Enable webcam</button>
+                            </React.Fragment>
+                          );
+                        } 
+                    })()
+                }
+            </div>
           </div>
         </div>
         <Footer/>
